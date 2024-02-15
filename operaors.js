625a05c57185dbfa52bff443840cacc6bf37488d
RAVNEETSA <160113152+RAVNEETSA@users.noreply.github.com>
2024-02-15 14:20:21 +0530

    Add files via upload

---
+++ operaors.js
@@ -0,0 +1,59 @@
+const student={
+    fullname:"ravneet",
+    age:20,
+    cpgp:8.0,
+    ispass:true,
+}
+console.log(student);
+
+// let a=5;
+//  let b=2;
+// console.log("a=",a," & b=",b) ;
+// console.log("5+2=",a+b);
+// console.log("a-b=",a-b);
+// console.log("a/b=",a/b);
+// console.log("a*b=",a*b);
+// console.log("a%b=",a%b);
+// console.log("a***b=",a**b);
+// console.log("")
+
+// uninary operator
+
+//  a++;
+// a=a-1;
+ //console.log("++a=",++a);
+//console.log("++a=",a," & b=",b) ;
+//console.log("a=",a);
+// console.log("a++=",a++);
+//console.log("++a=",a," & b=",b) ;
+// console.log("a=",a);
+
+//assiegment operator
+// let a=5;
+//  let b=2;
+//  a**=4;
+//  a+=4;
+//  a-=4;
+//  a/=4;
+//  console.log("a=",a);
+//  console.log("a=",a);
+//  console.log("a=",a);
+// console.log("a=",a);
+//comparision opearator
+let  a=5;
+let  b=6;
+//  console.log("a==b",a==b);//equalto
+//  console.log("a>=b",a>=b);//greaterthan
+//  console.log("a<=b",a<=b);//smallerthan
+//  console.log("a=!b",a>=!b);//noteqaulto
+//  console.log("a===b",a===b);//striclychecked
+//  console.log("a==!b",a==!b);
+//  console.log("a>b",a>b);//greater
+// console.log("a<b",a<b)//less
+//logical operator
+let= 5>6;
+let= 6>5;
+console.log("5>6 ||5<6",5>6||5<6);
+console.log("5>6 && 5<6",5>6 && 5<6);
+console.log("!(5 >6)=",!(5>6));
+
