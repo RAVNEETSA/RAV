f5bb5bef72f68813417f9001bfd696b8a043887f
RAVNEETSA <160113152+RAVNEETSA@users.noreply.github.com>
2024-02-15 14:19:49 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,42 @@
+//loops
+// forloops  pritnt 1 to 5 numbers
+// for(let count=1;count<=5;count++){
+//     // blockof code
+//     console.log("ravneet");
+// }
+// console.log("loop is completed");
+//  sum 2 calculate sum of n numbers
+//  let sum=0;
+//  for (let i=1; i<=10;i++){
+//     // sum=sum+i;  
+//     //   sum= 1+2=3,3+3=6,6+4=10,10+5=15
+//     console.log("i=", i);
+//  }
+//  console.log("sum=", sum);
+// while loop
+//   let i=1;
+// while(i<=5) {
+//     console.log("apna college");
+//     i++;
+// }
+// do while loop
+// for of loop
+// let str="college"; 
+// let size=0;
+// for(let val of str){  
+//     // iterator  
+//     console.log("val=",val);
+//     size++;
+// }
+// console.log("string size",size);
+// for in loop
+// let student ={
+//     name:"ravneet kaur",
+//     age:20,
+//     cpga:7.5,
+//     ispass:true,
+// };
+// for (let key in student){
+//     console.log("key=","value=", student[key]);
+// }
+// print all even numbers from 0 to 100
